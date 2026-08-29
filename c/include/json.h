a0a9ec2b8cb7e2636d1ca2090f5c777460942c4e
ikawamuk <ikawamuk@student.42.fr>
2026-08-30 04:34:35 +0900

    add delete and accesser definision

--- c/include/json.h
+++ c/include/json.h
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/29 22:08:43 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/08/30 03:47:36 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/08/30 04:33:48 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,7 @@ typedef struct s_json
 	union u_json_data
 	{
 		bool	bool_data;
-		int		int_data;
-		double	double_data;
+		double	number_data;
 		char	*string_data;
 		struct s_json_array
 		{
@@ -108,11 +107,119 @@ int		json_get_error_code(void);
 char	*json_get_error_message(int error_code);
 
 /**
- * @brief SONパース失敗時、エラーが発生したバイトオフセット（位置）を取得します。
+ * @brief JSONパース失敗時、エラーが発生したバイトオフセット（位置）を取得します。
  * @param _ なし
  * @return 0始まりのエラー発生位置。エラーがない場合は (size_t)-1
  * @note 先頭でエラーが起きた場合は 0 を返します。
  */
 size_t	json_get_error_position(void);
+
+
+/**
+ * @brief JSON要素が null であるか判定します。
+ * @param item 判定対象の JSON要素（NULL 許容）
+ * @return item が null の場合は true、それ以外および item が NULL の場合は false
+ * @note 安全のため、item が NULL ポインタであっても呼び出し可能です。
+ */
+bool	json_is_null(t_json const *const item);
+
+/**
+ * @brief JSON要素が true であるか判定します。
+ * @param item 判定対象の JSON要素（NULL 許容）
+ * @return item が true の場合は true、それ以外および item が NULL の場合は false
+ * @note 安全のため、item が NULL ポインタであっても呼び出し可能です。
+ */
+bool	json_is_true(t_json const *const item);
+
+/**
+ * @brief JSON要素が false であるか判定します。
+ * @param item 判定対象の JSON要素（NULL 許容）
+ * @return item が false の場合は true、それ以外および item が NULL の場合は false
+ * @note 安全のため、item が NULL ポインタであっても呼び出し可能です。
+ */
+bool	json_is_false(t_json const *const item);
+
+/**
+ * @brief JSON要素が数値（Number）であるか判定します。
+ * @param item 判定対象の JSON要素（NULL 許容）
+ * @return item が数値の場合は true、それ以外および item が NULL の場合は false
+ * @note 安全のため、item が NULL ポインタであっても呼び出し可能です。
+ */
+bool	json_is_number(t_json const *const item);
+
+/**
+ * @brief 数値の JSON要素から double 値を取得します。
+ * @param number 値を取得したい数値型の JSON要素
+ * @return 格納されている double 値。number が NULL または数値型でない場合は NaN
+ * @note 事前条件として json_is_number(number) が true であることを推奨します。
+ */
+double	json_get_number(t_json const *const number);
+
+/**
+ * @brief JSON要素が文字列（String）であるか判定します。
+ * @param item 判定対象の JSON要素（NULL 許容）
+ * @return item が文字列の場合は true、それ以外および item が NULL の場合は false
+ * @note 安全のため、item が NULL ポインタであっても呼び出し可能です。
+ */
+bool	json_is_string(t_json const *const item);
+
+/**
+ * @brief 文字列の JSON要素から文字列ポインタを取得します。
+ * @param string 値を取得したい文字列型の JSON要素
+ * @return 格納されている文字列（char *）のポインタ。string が NULL または文字列型でない場合は NULL
+ * @note 返されるポインタは内部保持データのため、呼び出し側で free しないでください。
+ */
+char	*json_get_string(t_json const *const string);
+
+/**
+ * @brief JSON要素が配列（Array）であるか判定します。
+ * @param item 判定対象の JSON要素（NULL 許容）
+ * @return item が配列の場合は true、それ以外および item が NULL の場合は false
+ * @note 安全のため、item が NULL ポインタであっても呼び出し可能です。
+ */
+bool	json_is_array(t_json const *const item);
+
+/**
+ * @brief 配列の要素数を取得します。
+ * @param array 要素数を取得したい配列型の JSON要素
+ * @return 配列に含まれる要素数。array が NULL または配列型でない場合は 0
+ * @note array が空の配列（[]）の場合も 0 を返します。
+ */
+size_t	json_get_array_size(t_json const *const array);
+
+/**
+ * @brief 配列から指定したインデックスの JSON要素を取得します。
+ * @param array 参照対象の配列型の JSON要素
+ * @param index 取得したい要素の 0 始まりのインデックス
+ * @return 指定したインデックスに位置する JSON要素へのポインタ。インデックスが範囲外、または array が NULL / 非配列の場合は NULL
+ * @note インデックスの有効範囲は 0 <= index < json_get_array_size(array) です。
+ */
+t_json	*json_get_array_element(t_json const *const array, size_t index);
+
+/**
+ * @brief JSON要素がオブジェクト（Object）であるか判定します。
+ * @param item 判定対象の JSON要素（NULL 許容）
+ * @return item がオブジェクトの場合は true、それ以外および item が NULL の場合は false
+ * @note 安全のため、item が NULL ポインタであっても呼び出し可能です。
+ */
+bool	json_is_object(t_json const *const item);
+
+/**
+ * @brief オブジェクト内に指定したキーが存在するか確認します。
+ * @param item object 検索対象のオブジェクト型の JSON要素
+ * @param  key 検索するキー文字列
+ * @return 指定したキーが存在する場合は true、存在しない場合や引数が不適切な場合は false
+ * @note 安全のため、object や key が NULL であっても呼び出し可能です。
+ */
+bool	*json_has_object_item(t_json const *const object, const char *key);
+
+/**
+ * @brief オブジェクトから指定したキーに対応する JSON要素を取得します。
+ * @param item object 参照対象のオブジェクト型の JSON要素
+ * @param  key 取得したいフィールドのキー文字列
+ * @return キーに対応する JSON要素へのポインタ。キーが存在しない、または object が NULL / 非オブジェクトの場合は NULL
+ * @note キーの比較は大文字・小文字を厳密に区別します。
+ */
+t_json	*json_get_object_item(t_json const *const object, const char *key);
 
 #endif
--- c/src/json_delete.c
+++ c/src/json_delete.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 00:21:41 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/08/30 01:06:43 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/08/30 04:11:44 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,28 @@
 
 void	json_delete(t_json *item)
 {
+	t_json	*next;
+
 	if (!item)
 		return ;
+	while (item)
+	{
+		next = NULL;
+		if (item->type == JSON_String)
+			free(item->_.string_data);
+		else if (item->type == JSON_Array)
+		{
+			json_delete(item->_.array_data.element);
+			next = item->_.array_data.next;
+		}
+		else if (item->type == JSON_Object)
+		{
+			free(item->_.object_data.key);
+			json_delete(item->_.object_data.value);
+			next = item->_.object_data.next;
+		}
+		free(item);
+		item = next;
+	}
+	return ;
 }
