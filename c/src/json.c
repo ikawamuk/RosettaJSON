d9af08d2f9bd47d27296c3509bd266dc22cb5618
ikawamuk <ikawamuk@student.42.fr>
2026-08-29 22:45:53 +0900

    change path

--- c/src/json.c
+++ c/src/json.c
@@ -6,11 +6,13 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/29 22:37:22 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/08/29 22:41:39 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/08/29 22:45:31 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "json.h"
 #include "json_private.h"
 
-static t_json_error	g_error;
+static t_json_error	g_error = {NULL, 0, NULL};
+
+
