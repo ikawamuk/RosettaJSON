ddce53e67bed0dc2065507374c6eb9c43231deea
ikawamuk <ikawamuk@student.42.fr>
2026-09-03 08:44:43 +0900

    add parse object

--- c/src/json_parse/parse_array.c
+++ c/src/json_parse/parse_array.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/02 22:17:12 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/09/03 08:43:44 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/09/03 08:44:19 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ int	parse_array(t_json *item, t_parse_buf *const buf)
 		--buf->offset;
 		return (fail(buf, NULL, INVALID_TOKEN));
 	}
+	--buf->offset;
 	if (parse_array_core(&tmp_array, buf) != 0)
 		return (-1);
 	if (!can_access_at_index(buf, 0) || parse_buf_at_offset(buf)[0] != ']')
